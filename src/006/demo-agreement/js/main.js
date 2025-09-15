e03af464c7e40e2dc6b6be950767f6000096c7e8
Dan Gilleland <dgilleland@nait.ca>
2025-09-15 09:41:28 -0600

    Selecting elements on the page

--- src/006/demo-agreement/js/main.js
+++ src/006/demo-agreement/js/main.js
@@ -8,3 +8,19 @@
 
 	Use the skills you have learned up to this point.
 */
+let agreementDate = document.querySelector('.date'); // get the element
+agreementDate.innerText = 'Sep 15, 2025'; // change the text inside
+
+// Get the parties to the contract/agreement
+let discloser = prompt('Enter the name of the disclosing party:');
+let receiver = prompt('Enter the name of the receiving party:');
+
+let element; // a general variable that I will re-use...
+element = document.querySelector('.disclosing-party');
+element.innerText = discloser;
+element = document.querySelector('.signature .disclosing-party');
+element.innerText = discloser;
+element = document.querySelector('.receiving-party');
+element.innerText = receiver;
+element = document.querySelector('.signature .receiving-party');
+element.innerText = receiver;
