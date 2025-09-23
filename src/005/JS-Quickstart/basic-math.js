140a0f7b3b751ddd5dc33c53f1b50764e22bf240
Dan Gilleland <dgilleland@nait.ca>
2025-09-23 09:35:01 -0600

    A flashback to math in JavaScript

--- src/005/JS-Quickstart/basic-math.js
+++ src/005/JS-Quickstart/basic-math.js
@@ -2,5 +2,12 @@ console.log('My basic-math.js has loaded');
 
 let number = .1 + .1;
 console.log(number);
-number = .1 + .1 + .1 + .1;
+number = .1 + .1 + .1;
 console.log(number);
+
+// What is the area of a circle?
+// Given a radius of 10, what is the area?
+let radius = 10;
+let area = 2 * Math.PI * radius * radius;
+//                       Math.pow(radius, 2);
+console.log(`The area of a circle with ${radius} radius is ${area}.`);
