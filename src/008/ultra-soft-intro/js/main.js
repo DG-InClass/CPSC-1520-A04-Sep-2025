9522428bfe96d53865deb736e404707ee7e32274
Dan Gilleland <dgilleland@nait.ca>
2025-09-23 08:38:49 -0600

    Added comments to code

--- src/008/ultra-soft-intro/js/main.js
+++ src/008/ultra-soft-intro/js/main.js
@@ -1,8 +1,18 @@
-let counter = 0;
+let counter = 0; // An initial value for my counter
+// function that will increment and display the counter's value
 const increment = function() {
     counter++;
     document.querySelector('div').innerText = `Clicked ${counter} times.`;
 }
 
+// To "wire up" my event listener, I follow two steps
+// 1) Reference the element I want to respond to.
 const theButton = document.querySelector('button');
+// 2) Add a "listener" for a specific event
 theButton.addEventListener('click', increment);
+//                         \event/,\callback/
+//                                  \ - it's the name of a function that
+//                                      will be called whenever the event
+//                                      occurs.
+//                                    - Another word for a callback is
+//                                      delegate
