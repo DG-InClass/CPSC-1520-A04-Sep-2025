8241a2df7168edca9ec8e58ceb98b61c054026db
Dan Gilleland <dgilleland@nait.ca>
2025-09-23 09:22:04 -0600

    Event demos

--- src/008/soft-intro/js/main.js
+++ src/008/soft-intro/js/main.js
@@ -11,8 +11,15 @@
 console.log('JavaScript file loaded!');
 
 const handleClick = function(event) {
+    // Any function that is intended as an "event handler"
+    // or "event listener" will be given an Event object
+    // when it is called by the browser.
     // TODO: Use for exploration purposes
     const target = event.target;
+    //                  \_____/ <- this is what DOM element the event
+    //                             is associated with
+    console.log(target.tagName); // Every HTML Element has a property called
+                                 // .tagName
     document.getElementById('feedback').innerText = `Clicked from ${target.tagName}`;
 }
 
@@ -23,6 +30,15 @@ heading.addEventListener('click', handleClick);
 //       Listen for the 'dblclick' event.
 //       Prompt the user for their name, then for their email,
 //       then output the information to the #feedback element.
+let theButton = document.getElementById('register');
+theButton.addEventListener('dblclick', function(evt) {
+    // Here is the code that will run when the dblclick event happens
+    let name = prompt('Please enter your name:');
+    let email = prompt('Please enter your email:');
+    let result = `Thank you ${name}. We sent a confirmation email to ${email}.`;
+    let outputTag = document.getElementById('feedback');
+    outputTag.innerText = result;
+});
 
 // TODO: Add another event listener for the heading, this time for
 //       the 'dblclick' event.
