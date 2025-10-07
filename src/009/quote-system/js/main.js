d0df0700d5d1488778e1a4c3c278a70f2d830839
Dan Gilleland <dgilleland@nait.ca>
2025-10-07 09:00:23 -0600

    Intro to form validation

--- src/009/quote-system/js/main.js
+++ src/009/quote-system/js/main.js
@@ -1,17 +1,19 @@
 
 document.querySelector('form')
     .addEventListener('submit', function(evt) {
-        evt.preventDefault();
-        const form = evt.target;
-        const formControls = form.elements;
+        evt.preventDefault(); // 👍 Always do this (for THIS course)
+        const form = evt.target; // for 'submit' events, this is always the <form> element
+        const formControls = form.elements; // these are all the controls on the form
 
-        console.log(formControls);
+        console.log(formControls); // good idea if you want to "explore" what controls are on the page/form
 
         // Output is being displayed as "plain text" in the browser
         const outputControl = formControls.breakdown; // <output name="breakdown" />
         let message = 'Contract Parameters Received...\n';
         outputControl.value = message;
 
+        let isValidInput = true; // Optimistic approach
+        let errorSummary = ""; // No problems to report (yet)
         /* Validation Expectations:
          *  - job duration must be greater than zero
          *  - interval selection must be made (hours vs. weeks)
@@ -33,12 +35,28 @@ document.querySelector('form')
         // const inputContractPeriod = formControls.interval; // RadioNodeList: <input name="interval" />
         const inputBaseRate = formControls['base-rate']; // <input name="base-rate" />
 
+        // Step 0) Validate user input
+        //      - job duration must be greater than zero
         let jobDuration = parseFloat(inputJobDuration.value);
-        message = `Job duration: ${jobDuration}`;
-        let inHours = inputRadioHours.checked;
-        let inWeeks = inputRadioWeeks.checked;
-        message = `\tinHours:${inHours} | inWeeks:${inWeeks}\n` + message;
-        outputControl.value += message;
+        if(isNaN(jobDuration)) {
+            isValidInput = false;
+            errorSummary += "Job duration is required.\n";
+        } else {
+            // Yes, it's a number, BUT
+            if(jobDuration <= 0) {
+                isValidInput = false;
+                errorSummary += "Job duration must be greater than zero.\n";
+            }
+        }
+
+
+        // The following are just samples of processing the input
+        // let jobDuration = parseFloat(inputJobDuration.value);
+        // message = `Job duration: ${jobDuration}`;
+        // let inHours = inputRadioHours.checked;
+        // let inWeeks = inputRadioWeeks.checked;
+        // message = `\tinHours:${inHours} | inWeeks:${inWeeks}\n` + message;
+        // outputControl.value += message;
 
 
         // Digital Assets (extra charges)
@@ -50,4 +68,12 @@ document.querySelector('form')
         const inputPerWordRate = formControls['per-word-rate']; // <input name="per-word-rate" />
         const inputStartDate = formControls['start-date']; // <input type="date" name="start-date" />
 
+        // Step 1) Decide if I process or report errors
+        if(isValidInput) {
+            // Step 2) Do all the processing of the user inputs
+            // TODO: Create the estimate...
+        } else {
+            // Step 3) Report that the user needs to try again
+            outputControl.value = errorSummary;
+        }
     });
