c538d8f875f80f5667c27f767d11575fc3e6ceec
Dan Gilleland <dgilleland@nait.ca>
2025-10-21 09:19:27 -0600

    Add comments to code

--- src/011/JS-Quickstart/a-backlog.js
+++ src/011/JS-Quickstart/a-backlog.js
@@ -18,8 +18,13 @@ let instructor = {
         const result = `If you have questions, you can contact me via MS Teams.
 - ${this.name}
   - ${this.githubProfile}\n`;
+        if(display) log(result);
+        return result;
     }
 }
+
+console.log(instructor);
+instructor.help(true);
 
 // 2. Date Object in JavaScript
 topic('Date Object in JavaScript');
@@ -44,7 +49,7 @@ log(pass);
 // 4. Functions as Parameters (Callbacks)
 topic('Functions as Parameters (Callbacks)');
 const runSomeCallback = function(callback, data) {
-    callback(data);
+    callback(data); // A single arg for inputs
 }
 const reverse = function(text) {
     text = text?.toString();
@@ -95,9 +100,11 @@ log(greet()); // Hello, Guest!
 log(greet("Alice")); // Hello, Alice!
 
 // 8. Closures
+//    The secret to understanding closures in JavaScript
+//    is to understand SCOPE in JavaScript.
 topic('Closures');
 function makeCounter() {
-    let count = 0;
+    let count = 0; // is "closed" in terms of "visibility"
     return function() {
         count++;
         return count;
