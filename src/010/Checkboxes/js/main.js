741499443156d75739a22b78dce2e99e17d92195
Dan Gilleland <dgilleland@nait.ca>
2025-10-24 14:23:31 -0600

    Looping through a NodeList

--- src/010/Checkboxes/js/main.js
+++ src/010/Checkboxes/js/main.js
@@ -1,1 +1,17 @@
 // Enter JavaScript for the exercise here...
+// 1) Listen for changes to the <input type="checkbox" name="toggle" />
+const toggleAll = document.querySelector('[name=toggle]');
+toggleAll.addEventListener('change', function (evt) {
+    const target = evt.target; // our <input type="checkbox" name="toggle" />
+    console.log(`main toggle is ${target.checked ? 'on' : 'off'}`);
+    //                            \___ ternary expression ____/
+
+    const allCheckboxes = document.querySelectorAll('[name=message]');
+    console.log(allCheckboxes); // a NodeList
+
+    let index = 0;
+    while (index < allCheckboxes.length) {
+        allCheckboxes[index].checked = target.checked;
+        index += 1;
+    }
+});
