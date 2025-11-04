428eeba2353a2cb73e5526e6f3fc338a6f6fb031
Dan Gilleland <dgilleland@nait.ca>
2025-11-04 10:27:29 -0700

    Comment on code

--- src/012/JS-Quickstart/another-array-note.js
+++ src/012/JS-Quickstart/another-array-note.js
@@ -2,9 +2,17 @@
 console.clear();
 console.log('Sorting Arrays');
 console.log('==============');
+const show = console.log;
 
 // A) Sorting an array of numbers
 let numbers = [2, 8, 4, 6, 3, 7, 12, 1];
+let numbersSorted = numbers.sort(numberCompare);
+show(numbersSorted);
+// The key to sorting is building a comparison function
+function numberCompare(a, b) {
+    return parseFloat(a) - parseFloat(b);
+}
+
 
 // B) Sorting an array of strings
 let strings = 'sorting an array of strings'.split(' ');
