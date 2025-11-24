129170ba351bf3cb5c7f0e612294eb70996d82c3
Dan Gilleland <dgilleland@nait.ca>
2025-11-24 09:44:26 -0700

    Introduction to classes

--- src/015/Rectangles/js/main.js
+++ src/015/Rectangles/js/main.js
@@ -1,3 +1,43 @@
+// A class is a blueprint for what an object looks like (information)
+// and how the object behaves (instructions)
+
+const Rectangle = class {
+    // Information (properties/fields)
+    #created;   // Private property - #
+    height;     // Public property
+    width;      // Public property
+
+    constructor(height, width) {
+        // The job of a constructor is to make sure
+        // the object's properties/fields have
+        // meaningful value
+        this.height = height;
+        this.width = width;
+        this.#created = new Date(); // when this object was created
+        Rectangle.#count++; // increment each time a rectangle is created
+    }
+
+    report() {
+        console.log(`This box was made on ${this.#created.toLocaleTimeString()}`);
+    }
+
+    // We use "getters" to calculate information from whatever other data is in our object
+    get area() {
+        return this.width * this.height;
+    }
+
+    get perimeter() {
+        return (this.width + this.height) * 2;
+    }
+
+    // Static members of the Rectangle class
+    static #count = 0; // Private, static variable/field
+
+    // A "getter" function
+    static get count() {
+        return Rectangle.#count;
+    }
+}
 
 const shapes = [];
 
@@ -7,10 +47,12 @@ document.querySelector('button').addEventListener('click', (evt) => {
     const h = 5;
     
     // Create the Rectangle object.
-    const box = new Rectangle(h, w);
+    const box = new Rectangle(h, w); // Instantiate the class
     
     shapes.push(box);
     console.log('box created:', box);
+    console.log(`I have ${Rectangle.count} boxes.`);
+
     box.report(); // call my method/function of my Rectangle instance
     console.log(`There are ${Rectangle.count} boxes so far.`);
 });
